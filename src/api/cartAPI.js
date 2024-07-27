28296b29c10aa2ecce82d9e7df370d684bd5cd06
Nguyen Tien Dat <maxdat2003@gmail.com>
2024-07-27 11:53:15 +0700

    handle_callAPI_getMyCart_updateCartItem_deleteCartItem_in_page_cart

--- src/api/cartAPI.js
+++ src/api/cartAPI.js
@@ -8,6 +8,18 @@ const cartAPI = {
   cartQuantity: (userId) => {
     const url = `/user/cart-quantity/${userId}`
     return axiosClient.get(url)
+  },
+  getMyCart: (userId) => {
+    const url = '/user/my-cart'
+    return axiosClient.get(url)
+  },
+  updateCartItem: (userId, reqBody) => {
+    const url = '/user/update-cart-quantity'
+    return axiosClient.post(url, reqBody)
+  },
+  deleteCartItem: (userId, bookId) => {
+    const url = `/user/delete-cart-item/${bookId}`
+    return axiosClient.delete(url)
   }
 }
 export default cartAPI
